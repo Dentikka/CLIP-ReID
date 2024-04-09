56293049f338e9db9e9d94e1507def73961a0cf1
Dentikka <denis@shveykin.com>
2024-04-09 22:12:57 +0300

    add attribute mask into SupConLoss

--- loss/supcontrast.py
+++ loss/supcontrast.py
@@ -12,11 +12,15 @@ class SupConLoss(nn.Module):
         super(SupConLoss, self).__init__()
         self.device = device
         self.temperature = 1.0
-    def forward(self, text_features, image_features, t_label, i_targets): 
+    def forward(self, text_features, image_features, t_attributes, i_attributes, t_label, i_targets): 
         batch_size = text_features.shape[0] 
-        batch_size_N = image_features.shape[0] 
-        mask = torch.eq(t_label.unsqueeze(1).expand(batch_size, batch_size_N), \
+        batch_size_N = image_features.shape[0]
+
+        identity_mask = torch.eq(t_label.unsqueeze(1).expand(batch_size, batch_size_N), \
             i_targets.unsqueeze(0).expand(batch_size,batch_size_N)).float().to(self.device) 
+        attribute_mask = torch.eq(t_attributes.unsqueeze(1).expand(batch_size, batch_size_N, -1), \
+            i_attributes.unsqueeze(0).expand(batch_size,batch_size_N, -1)).mean(dim=2).float().to(self.device)
+        mask = (identity_mask + attribute_mask) * 0.5
 
         logits = torch.div(torch.matmul(text_features, image_features.T),self.temperature)
         # for numerical stability

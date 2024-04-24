19f15269099e8b4c137ab08302b690cb1223721f
Dentikka <denis@shveykin.com>
2024-04-24 16:24:29 +0300

    add attributes in cross-entropy loss

--- loss/softmax_loss.py
+++ loss/softmax_loss.py
@@ -1,23 +1,18 @@
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class CrossEntropyLabelSmooth(nn.Module):
-    """Cross entropy loss with label smoothing regularizer.
-
-    Reference:
-    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
-    Equation: y = (1 - epsilon) * y + epsilon / K.
+class CrossEntropyAttributes(nn.Module):
+    """Cross entropy loss adjusted by attribute information.
 
     Args:
-        num_classes (int): number of classes.
-        epsilon (float): weight.
+        attributes_data: DataFrame with attributes annotations for each class
     """
 
-    def __init__(self, num_classes, epsilon=0.1, use_gpu=True):
-        super(CrossEntropyLabelSmooth, self).__init__()
-        self.num_classes = num_classes
-        self.epsilon = epsilon
+    def __init__(self, attributes_data, use_gpu=True):
+        super(CrossEntropyAttributes, self).__init__()
+        self.attributes_data = attributes_data
         self.use_gpu = use_gpu
         self.logsoftmax = nn.LogSoftmax(dim=1)
 
@@ -27,11 +22,20 @@ class CrossEntropyLabelSmooth(nn.Module):
             inputs: prediction matrix (before softmax) with shape (batch_size, num_classes)
             targets: ground truth labels with shape (num_classes)
         """
+        N = len(inputs)
+        num_classes, num_attributes = self.attributes_data.shape
+
         log_probs = self.logsoftmax(inputs) 
-        targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1) 
+        
+        attributes_target = self.attributes_data.loc[targets].values
+        attributes_target = np.broadcast_to(attributes_target[:, None, :], (N, num_classes, num_attributes))
+        attributes_all = self.attributes_data.values
+        attributes_all = np.broadcast_to(attributes_all[None, :, :], (N, num_classes, num_attributes))
+
+        attributes_mask = (attributes_target == attributes_all).sum(axis=-1) / num_attributes
+
         if self.use_gpu: targets = targets.cuda()
-        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
-        loss = (- targets * log_probs).mean(0).sum()
+        loss = (- attributes_mask * log_probs).mean(0).sum()
         return loss
 
 class LabelSmoothingCrossEntropy(nn.Module):

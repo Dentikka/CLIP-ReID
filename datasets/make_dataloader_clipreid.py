fix keypoints normalization

datasets/make_dataloader_clipreid.py
@@ -34,7 +34,7 @@ def train_collate_fn(batch):
     pids = torch.tensor(pids, dtype=torch.int64)
     viewids = torch.tensor(viewids, dtype=torch.int64)
     camids = torch.tensor(camids, dtype=torch.int64)
-    keypoints = torch.tensor(np.stack(keypoints))
+    keypoints = torch.tensor(np.stack(keypoints)) if keypoints[0] is not None else None
     return torch.stack(imgs, dim=0), pids, camids, viewids, keypoints
 
 def val_collate_fn(batch):

model/make_model_clipreid.py
@@ -59,7 +59,6 @@ class KeypointsEncoder(nn.Module):
             nn.Linear(100, 100),
             nn.ReLU(),
             nn.BatchNorm1d(100),
-            nn.Dropout(0.5),
             nn.Linear(100, out_features)
         )
 
@@ -146,7 +145,7 @@ class build_transformer(nn.Module):
             img_feature = nn.functional.avg_pool2d(image_features, image_features.shape[2:4]).view(x.shape[0], -1) 
             img_feature_proj = image_features_proj[0]
             if keypoints is not None:
-                keypoints /= torch.tensor(x.shape[2:]).cuda()
+                keypoints /= torch.tensor(x.shape[3:1:-1]).cuda()
                 kps_feature_proj = self.keypoints_encoder(keypoints)
                 img_feature_proj = torch.cat([img_feature_proj, kps_feature_proj], dim=1)
                 img_feature_proj = self.image_keypoints_projector(img_feature_proj)